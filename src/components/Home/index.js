55122d5e23e6fb572bc2a2e581534dda65306525
João Damião <thejoaodamiao@gmail.com>
2022-01-10 18:03:48 -0300

    criar componente Home

---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from './styles';
+
+
+const Home = () => {
+    return (
+        <div>
+            <Container>
+                <h1>Home</h1>
+            </Container>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/components/Home/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: black;
+
+    h1{
+        color: aliceblue;
+        font-size: 5em;
+        
+    }
+
+`
